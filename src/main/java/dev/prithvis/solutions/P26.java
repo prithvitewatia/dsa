e505af538c3b07a34840cefc9296f5fad76b5aab
prithvi <prithvisinghtewatia@gmail.com>
2022-11-11 09:45:32 +0530

    Solved P26

--- src/main/java/dev/prithvis/solutions/P26.java
+++ src/main/java/dev/prithvis/solutions/P26.java
@@ -1,11 +1,14 @@
 package dev.prithvis.solutions;
 
 public class P26 {
-    public int removeDuplicates(int[] nums){
-        int count=0;
-        for(int i=1; i<nums.length;i++){
-            if(nums[i]!=nums[count]){
-                nums[count+1]=nums[i];
+    public int removeDuplicates(int[] nums) {
+        if(nums.length==0){
+            return 0;
+        }
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[count-1]) {
+                nums[count] = nums[i];
                 count++;
             }
         }
