492571c6049d75c866e218be2b7929a5e108a8c1
prithvi <prithvisinghtewatia@gmail.com>
2022-12-26 20:02:03 +0530

    Initialized solution only once

--- src/test/java/dev/prithvis/solutions/P931Test.java
+++ src/test/java/dev/prithvis/solutions/P931Test.java
@@ -1,47 +1,28 @@
 package dev.prithvis.solutions;
 
-import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.Random;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class P931Test {
-    P931 solution;
-    @Before
-    public void beforeMethod(){
-        solution=new P931();
-    }
+    P931 solution = new P931();
+
     @Test
-    public void test1(){
-        int[][] input={
-                {2,1,3},
-                {6,5,4},
-                {7,8,9}
+    public void test1() {
+        int[][] input = {
+                {2, 1, 3},
+                {6, 5, 4},
+                {7, 8, 9}
         };
-        assertEquals(13,solution.minFallingPathSum(input));
-    }
-    @Test
-    public void test2(){
-        int[][] input={
-                {-19,57},
-                {-40,-5},
-        };
-        assertEquals(-59,solution.minFallingPathSum(input));
+        assertEquals(13, solution.minFallingPathSum(input));
     }
 
-    private int[][] generateRandom2dArray(int length,int breadth,int minValue,int maxValue){
-        assert length >=0 && breadth >=0;
-        int[][] randomArray=new int[length][breadth];
-        Random random=new Random();
-        for(int i=0;i<length;i++){
-            for(int j=0;j<breadth;j++){
-                randomArray[i][j]= random.nextInt(minValue,maxValue);
-            }
-        }
-        System.out.println(Arrays.deepToString(randomArray));
-        return randomArray;
+    @Test
+    public void test2() {
+        int[][] input = {
+                {-19, 57},
+                {-40, -5},
+        };
+        assertEquals(-59, solution.minFallingPathSum(input));
     }
 }
