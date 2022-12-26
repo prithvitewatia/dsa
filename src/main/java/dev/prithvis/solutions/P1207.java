96bc35cb67bb70e23037ff4bf2df35c3dc58605e
prithvi <prithvisinghtewatia@gmail.com>
2022-12-26 20:18:11 +0530

    Solved problem 1207

--- src/main/java/dev/prithvis/solutions/P1207.java
+++ src/main/java/dev/prithvis/solutions/P1207.java
@@ -1,20 +1,15 @@
 package dev.prithvis.solutions;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class P1207 {
     public boolean uniqueOccurrences(int[] arr) {
-        Map<Integer,Integer> freqMap=new HashMap<>();
-        for(int num:arr){
-            freqMap.put(num,freqMap.getOrDefault(num,0)+1);
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for (int num : arr) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
         }
-        List<Integer> sortedOccurrence= freqMap.values().stream().sorted().toList();
-        for (int i=0;i < sortedOccurrence.size()-1;i++){
-            if(sortedOccurrence.get(i)==sortedOccurrence.get(i+1)){
-                return  false;
-            }
-        }
-        return true;
+        Collection<Integer> mapValues = freqMap.values();
+        Set<Integer> uniqueValues = new HashSet<>(mapValues);
+        return mapValues.size() == uniqueValues.size();
     }
 }
